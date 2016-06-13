fa4b02420c33116e33cefb434429b28d655b9e62
Tsvetelina Ivanova <tsvetelina.ivanova@telerik.com>
2016-06-13 13:16:12 +0000

    Changes made by Tsvetelina Ivanova (tsvetelina.ivanova@telerik.com).

--- romance-ns/app/shared/view-model/main-view-model.js
+++ romance-ns/app/shared/view-model/main-view-model.js
@@ -1,17 +1,17 @@
 var observable = require("data/observable");
 var observableArray = require("data/observable-array");
-var ViewModelItem = (function () {
-    function ViewModelItem(title, info) {
-        this.title = title;
-        this.info = info;
-    }
-    return ViewModelItem;
-})();
-exports.ViewModelItem = ViewModelItem;
+var device_item_1 = require("../models/device-item");
 var items = new observableArray.ObservableArray();
-for (var i = 0; i < 20; i++) {
-    items.push(new ViewModelItem("Item " + i, "This is the item with number " + i + "."));
-}
+items.push(new device_item_1.DeviceItem({
+    "type": "actor",
+    "info": "Light dimmer",
+    "mode": "range",
+    "kind": "light",
+    "min": 0,
+    "max": 1023,
+    "queryParam": "A1",
+    "currentValue": "123"
+}));
 exports.mainViewModel = new observable.Observable();
 exports.mainViewModel.set("items", items);
 //# sourceMappingURL=main-view-model.js.map
