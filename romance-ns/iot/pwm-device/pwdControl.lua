49b76d1fd4b3b297e7ad9295524b9cd28de6f7a2
Assi <Ivomir.Assi@telerik.com>
2016-06-13 13:42:23 +0300

    Fix bug with slider

--- romance-ns/iot/pwm-device/pwdControl.lua
+++ romance-ns/iot/pwm-device/pwdControl.lua
@@ -1,9 +1,8 @@
-
 pwm= require("pwm");
 
 print(wifi.sta.getip()) -- Dynamic IP Address
 led1 = 1
-evenCounter = 0;
+evenCounter = 1;
 currentLightValue = 0
 pwm.setup(1,1000,0);
 pwm.start(1);
@@ -11,6 +10,8 @@ pwm.start(1);
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
+        print(request);
+        
         local buf = "";
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
         if(method == nil)then
@@ -22,10 +23,11 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-        local pesho = _GET.pin;
-		if (evenCounter %2 == 0)then
-			buf = buf.."<meta http-equiv='refresh' content='0'>";
-		end
+        isEven = ((evenCounter % 2) == 0);
+        print(isEven)
+	if(isEven)then
+	    buf = buf.."<meta http-equiv='refresh' content='0'>";
+	end
         buf = buf.."<h1>ESP8266 Web Server</h1>";
         buf = buf.."<form>"
         buf = buf.."<input type='range' name='pin' value="..currentLightValue.." min='10' max='1023' step='5'/>"
@@ -34,13 +36,13 @@ srv:listen(80,function(conn)
         -- buf = buf.."<a href=\'?pin=0\'><button>OFF</button></a>"
         buf = buf.."</form>"
         if(_GET.pin)then
-              gpio.write(1, gpio.HIGH);
               pwm.setduty(1,_GET.pin);
-			  currentLightValue = _GET.pin;
-			  evenCounter++;
+	      currentLightValue = _GET.pin;
+              print(evenCounter);
         end
         client:send(buf);
         client:close();
+         evenCounter = evenCounter + 1;
         collectgarbage();
     end)
 end)
